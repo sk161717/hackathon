29c4d1455a87474a9c4e5ceec76ac17fe35be5e3
片山峻佑 <katayamashunsuke@katayamashunyuus-MacBook-Air.local>
2021-07-24 18:12:52 +0900

    egg and frog

--- app/static/main.js
+++ app/static/main.js
@@ -1,7 +1,18 @@
+
 function LoadPicture(){
     var img_element=document.createElement("img");
-    img_element.id="fish";
-    img_element.src='/static/picture/otama_1_normal.png';
+    //tmp
+    query_string="frog"
+
+    img_element.className="image";
+    img_element.src='/static/picture/'+query_string+'.png';
+    if (query_string=="egg") {
+        img_element.id="egg";
+    }else if(query_string=="frog"){
+        img_element.id="frog";
+    }else{
+        img_element.id="fish";
+    }
     img_element.width=200;
     img_element.height=200;
     document.getElementsByClassName("parent_picture")[0].appendChild(img_element);
@@ -12,8 +23,27 @@ var loop=true;
 var easing='easeInOutQuad';
 var direction='alternate';
 
-
+var egg=document.getElementById("egg");
 var fish=document.getElementById("fish");
+var frog=document.getElementById("frog");
+
+var eggTimeline=anime.timeline({
+    loop,
+    direction:'alternate'
+})
+eggTimeline.add({
+    targets: egg,
+    translateX:30,
+    easing
+}).add({
+    targets:egg,
+    translateX:-30,
+    easing
+}).add({
+    targets:egg,
+    translateX:0,
+    easing
+})
 
 var fishTimeline=anime.timeline({
     loop,
@@ -34,4 +64,15 @@ fishTimeline.add({
     translateY: '-80',
     translateX: 470,
     easing
+  })
+
+  var frogTimeline=anime.timeline({
+      loop,
+      direction:'alternate'
+  })
+  frogTimeline.add({
+      targets:frog,
+      translateY:200,
+      easing:'spring',
+      duration:200
   })
